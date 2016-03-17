02e6214e35cf5db44dc3d862288634b19b63aa99
John Bellone <jbellone@bellone.us>
2016-03-16 23:53:35 -0400

    Fix function for cached_file.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jbellone@bloomberg.net'
 license 'Apache 2.0'
 description 'Library cookbook which installs and configures rubyzip.'
 long_description 'Library cookbook which installs and configures rubyzip.'
-version '0.3.1'
+version '0.3.3'
 
 supports 'centos'
 supports 'redhat'
